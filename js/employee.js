b2e0318773c2ddd5bb753249bc052afb7599e59e
RVovk7 <RVovk7@gmail.com>
2017-12-24 03:01:19 +0200

    alpha 0.1.1 fix

--- js/employee.js
+++ js/employee.js
@@ -1,22 +1,20 @@
 'use strict';
 ///////// get and parse Json file
 let xhr = new XMLHttpRequest();
-xhr.open("GET", "http://localhost:1111");
+xhr.open("GET", "http://wolf-mac.local:1111");   //warning change to "http://localhost:1111"
 
-xhr.onreadystatechange = function(){
+xhr.onreadystatechange = () =>{
  if(xhr.status === 200 && xhr.readyState === 4) {
   let responseJson = JSON.parse(xhr.responseText);
 createUi(responseJson);
  }
- 
 }
 xhr.send();
- ////////////
   //////////// output empoloyee to html
 function createUi(arrUsers){
  let parentDiv = document.getElementById('emp_wr');
  
-arrUsers.forEach(function(el,i){
+arrUsers.forEach((el,i) => {
   let currentDiv = document.createElement('div');
   currentDiv.classList.add("row");
   currentDiv.innerHTML =  el.firstName +' '+ el.lastName;
@@ -32,9 +30,7 @@ arrUsers.forEach(function(el,i){
 function boss_id(arrUsers) {
 	event.target.value = arrUsers[event.target.id];
 	let user = event.target.value;
-let slaves =  arrUsers.filter(function(el){
-	return  el.bossid > user.bossid;
-});  
+let slaves =  arrUsers.filter(el => el.bossid > user.bossid);  
  
  let div_slave = document.getElementById("slaves_list");
 div_slave.innerHTML = user.firstName +" " + user.lastName + ' - slaves:';
@@ -45,12 +41,10 @@ if (slaves.length == 0){
 div_slave.appendChild(ol_list);
 ol_list.type = "I";
 ol_list.style.border = "solid 0.1px silver";
-slaves.forEach(function(el){
+slaves.forEach(el => {
 let current_slave = document.createElement('li');
 current_slave.classList.add =('sl_row')
   current_slave.innerHTML = el.firstName +' '+ el.lastName;
   	ol_list.appendChild(current_slave);
 });
-
 }
-  ////////////
