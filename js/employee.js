f9451c82c5d3c276f593d86e195c8504ade5381e
RVovk7 <RVovk7@gmail.com>
2018-01-26 11:28:27 +0200

    With gide bug

--- js/employee.js
+++ js/employee.js
@@ -9,14 +9,10 @@ xhr.onreadystatechange = () => {
     all.addEventListener('click', () => {
       createUi(responseJson);
     })
-
-
-    // populateDatabase(responseJson);
-
   }
 }
 xhr.send();
-
+let objS = {}, count = 0,arrS = [];
 function createUi(arrUsers) {
   let parentDiv = document.getElementById('emp_wr');
 
@@ -27,9 +23,6 @@ function createUi(arrUsers) {
     let name = document.createElement('div');
     name.classList.add("name");
     name.innerHTML = ` ${el.firstName}  ${el.lastName} `;
-
-
-    
 
     let avatar = document.createElement('div');
     avatar.classList.add("avatar");
@@ -43,57 +36,85 @@ function createUi(arrUsers) {
     detail.classList.add('btn');
     detail.innerHTML = 'Detail';
 
-
-    currentDiv.appendChild(name);
-    currentDiv.appendChild(avatar);
-    avatar.appendChild(img);
-
-
-
+    detail.id = i;
     detail.addEventListener('click', det);
 
     function det() {
-      let hideD = document.createElement('button');
-      hideD.classList.add('btn');
-      hideD.innerHTML = 'hide';
-
-      currentDiv.appendChild(hideD);
       let inform = document.createElement('div');
-      inform.classList.add('inform');
-      inform.innerHTML = `UserName:${el.userName} <br> Email:${el.email} <br>  BirthDate:${el.birthDate}`;
-      currentDiv.appendChild(inform);
-      detail.style = 'display:none';
+    let delSlave = document.querySelectorAll('.inform');
+    delSlave.id = event.target.id;
+    arrS = [].slice.call(event.target.id);
+   
+      if (detail.innerHTML == 'Detail') {
+        arrS.forEach((e,i) =>{
+          objS[e]=count++;
+          });
+        
+       
+        detail.innerHTML = 'Hide';
+        inform.classList.add('inform');
+        inform.innerHTML = `UserName: ${el.userName} <br> Email: ${el.email} <br>  BirthDate: ${el.birthDate}`;
+        currentDiv.appendChild(inform);
+        boss_id(arrUsers);
+       
+      }
+      else {
+       // console.log(objS[count]);
+           detail.innerHTML = 'Detail';
+        console.log(delSlave.id);
+         
+         console.log(objS);
+         // console.log(filtS);
+    
+        console.log(delSlave);
+        //debugger;
+        
+          //delSlave[objS[delSlave.id]].remove();
+        
+           
+         
+          
+      
+    }
 
     }
+    
+    ////structure create
+    currentDiv.appendChild(name);
+    currentDiv.appendChild(avatar);
+    avatar.appendChild(img);
     showD.appendChild(detail);
     currentDiv.appendChild(showD);
     parentDiv.appendChild(currentDiv);
-    currentDiv.id = i;
   });
 
-  parentDiv.addEventListener('click', () => {
-    boss_id(arrUsers);
-  });
 }
-//////////// output slaves to html
+//////////// output slaves list
 function boss_id(arrUsers) {
-  event.target.value = arrUsers[event.target.id];
-  let user = event.target.value;
+  
+  let user = arrUsers[event.target.id];
   let slaves = arrUsers.filter(el => el.bossid > user.bossid);
-
-  let div_slave = document.getElementById("slaves_list");
-  div_slave.innerHTML = user.firstName + " " + user.lastName + ' - slaves:';
+  
+  let slave =  document.getElementsByClassName('inform');
+   let divSlave = document.createElement('div');
+   divSlave.classList.add('slaves');
+  
+  
+  slave =  document.getElementsByClassName('inform');
+  divSlave.innerHTML = `Slaves list:`;
   let ol_list = document.createElement('ol');
+  
   if (slaves.length == 0) {
     ol_list.innerHTML = "No one slave (";
   }
-  div_slave.appendChild(ol_list);
+  
+  divSlave.appendChild(ol_list);
+  slave[slave.length-1].appendChild(divSlave);
   ol_list.type = "I";
-  ol_list.style.border = "solid 0.1px silver";
   slaves.forEach(el => {
     let current_slave = document.createElement('li');
     current_slave.classList.add = ('sl_row')
-    current_slave.innerHTML = el.firstName + ' ' + el.lastName;
+    current_slave.innerHTML = `${el.firstName} ${el.lastName}`;
     ol_list.appendChild(current_slave);
 
   });
